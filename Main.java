8fe7e27c02268367f75a9cfce97db384cce5e027
Diego Muñoz <157607003+diegostrg@users.noreply.github.com>
2025-04-21 12:32:39 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,30 +1,35 @@
-import java.util.function.Function ;
 import java.util.List;
+import java.util.function.Function;
 
-
-
-public class Main{
+public class Main {
 
     static Function<Integer, String> unidades = (n) -> {
-
-            List<String> digitos = List.of("cero", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho","nueve");
-            return digitos.get(n);
-       
+        List<String> digitos = List.of("cero", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve");
+        return digitos.get(n);
     };
 
     static Function<Integer, String> especiales = (n) -> {
+        return List.of("diez", "once", "doce", "trece", "catorce", "quince", "dieciséis", "diecisiete", "dieciocho", "diecinueve").get(n - 10);
+    };
 
+    static Function<Integer, String> decenas = (n) -> {
 
-        return List.of("diez", "once", "doce", "trece", "catorce", "quince", "dieciseis", "diecisiete", "deiciocho", "diecinueve", "veinte").get(n);
-
-};
-
+    String resto = "";
+    if(n % 10 != 0) {
+    resto = " y " + unidades.apply(n % 10);
+    }
+        return List.of("", "", "veinte", "treinta", "cuarenta", "cincuenta", "sesenta", "setenta", "ochenta", "noventa").get(n / 10) + resto;
+    };
 
     public static void main(String[] args) {
-        final Integer num = 14;
-        if(num < 10)
-        System.out.println(unidades.apply(num));
-        else
-        System.out.println(especiales.apply(num));
+        final Integer num = 34;
+
+        if (num < 10) {
+            System.out.println(unidades.apply(num));
+        } else if (num >= 10 && num < 20) {
+            System.out.println(especiales.apply(num));
+        } else if (num >= 20 && num < 100) {
+            System.out.println(decenas.apply(num));
+        }
     }
 }

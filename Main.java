3aa5c2eed094ee4a1e57d6e5d96d0a21e1197cc9
Daniel Avila <151478778+dsavilam@users.noreply.github.com>
2025-04-21 11:39:48 -0500

    PUTO JAVIX

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Hola");
+import java.util.function.Function ;
+
+class Main {
+    public static void main(String[] args) {
+        Function<Integer, String> funcion = (n) -> {
+            if (n % 2 == 0)
+                return "El Numero" + n + "es par";
+            else
+                return "El Numero" + n + "es impar";
+        };
+        System.out.println(funcion.apply(10));
+        System.out.println(funcion.apply(7));
     }
 }

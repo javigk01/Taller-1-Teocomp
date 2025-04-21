eaaba93f6e741369f61f64d021ec32efe2a9dde6
Javier Felipe Aldana Jaramillo <al_javier@javeriana.edu.co>
2025-04-21 12:22:23 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,30 @@
 import java.util.function.Function ;
+import java.util.List;
 
-class Main {
+
+
+public class Main{
+
+    static Function<Integer, String> unidades = (n) -> {
+
+            List<String> digitos = List.of("cero", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho","nueve");
+            return digitos.get(n);
+       
+    };
+
+    static Function<Integer, String> especiales = (n) -> {
+
+
+        return List.of("diez", "once", "doce", "trece", "catorce", "quince", "dieciseis", "diecisiete", "deiciocho", "diecinueve", "veinte").get(n);
+
+};
+
+
     public static void main(String[] args) {
-        Function<Integer, String> funcion = (n) -> {
-            if (n % 2 == 0)
-                return "El Numero" + n + "es par";
-            else
-                return "El Numero" + n + "es impar";
-        };
-        System.out.println(funcion.apply(10));
-        System.out.println(funcion.apply(7));
+        final Integer num = 14;
+        if(num < 10)
+        System.out.println(unidades.apply(num));
+        else
+        System.out.println(especiales.apply(num));
     }
 }
